d4f72616af1637257e60a1bf45a1cb9934b4a729
ludovic-str <ludovic.sutter@epitech.eu>
2023-01-31 10:48:28 +0100

    Feat: update .env with oauth

--- back/src/env.ts
+++ back/src/env.ts
@@ -7,6 +7,9 @@ const ENV = {
   clientUrl: process.env.CLIENT_URL || "localhost",
   clientPort: process.env.CLIENT_PORT || "3000",
   secret: process.env.SECRET as string,
+  googleClientId: process.env.GOOGLE_CLIENT_ID as string,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  googleRedirectUrl: process.env.GOOGLE_REDIRECT_URL as string,
 };
 
 export default ENV;
